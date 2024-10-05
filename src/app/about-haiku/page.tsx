961fdf8671404be6b8e364faabde8249660b5b07
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-10-05 12:41:25 +0530

    feat: dummy about haiku page

--- src/app/about-haiku/page.tsx
+++ src/app/about-haiku/page.tsx
@@ -1,7 +1,67 @@
 import React from "react";
+import { BentoGrid, BentoGridItem } from "@/components/ui/bento-grid";
+import {
+  ArrowRightSquare,
+  ClipboardCopy,
+  File,
+  Signature,
+  TableColumnsSplit,
+} from "lucide-react";
 
 const AboutHaikuPage = () => {
-  return <div>AboutHaikuPage</div>;
+  return (
+    <div className="h-full w-full flex items-center justify-center py-8">
+      <BentoGrid className="max-w-4xl mx-auto">
+        {items.map((item, i) => (
+          <BentoGridItem
+            key={i}
+            title={item.title}
+            description={item.description}
+            header={item.header}
+            icon={item.icon}
+            className={i === 3 || i === 6 ? "md:col-span-2" : ""}
+          />
+        ))}
+      </BentoGrid>
+    </div>
+  );
 };
 
 export default AboutHaikuPage;
+
+const Skeleton = () => (
+  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
+);
+const items = [
+  {
+    title: "The Dawn of Innovation",
+    description: "Explore the birth of groundbreaking ideas and inventions.",
+    header: <Skeleton />,
+    icon: <ClipboardCopy className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "The Digital Revolution",
+    description: "Dive into the transformative power of technology.",
+    header: <Skeleton />,
+    icon: <File className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "The Art of Design",
+    description: "Discover the beauty of thoughtful and functional design.",
+    header: <Skeleton />,
+    icon: <Signature className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "The Power of Communication",
+    description:
+      "Understand the impact of effective communication in our lives.",
+    header: <Skeleton />,
+    icon: <TableColumnsSplit className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "The Pursuit of Knowledge",
+    description: "Join the quest for understanding and enlightenment.",
+    header: <Skeleton />,
+    icon: <ArrowRightSquare className="h-4 w-4 text-neutral-500" />,
+  },
+];
