83dddfbd1db227aa9030a5b2f8865094a7bc7094
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-10-10 20:04:43 +0530

    fix: login password validation

--- src/types/types.ts
+++ src/types/types.ts
@@ -38,8 +38,8 @@ export const registerFormSchema = z.object({
     email: z.string().email({
       message: "Please provide valid email.",
     }),
-    password: z.string().min(6, {
-      message: "Password must be at least 6 characters.",
+    password: z.string().min(1, {
+      message: "Password is required.",
     }),
   });
 
